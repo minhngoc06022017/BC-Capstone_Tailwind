fabd4c100f1295ac50991601a936cfc4c32d8be3
Bao Vo <bao.volam91105@hcmut.edu.vn>
2025-05-22 12:15:25 +0700

    updateBao

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 module.exports = {
-    content: [
-        "./index.html", // Quét file index.html ở thư mục gốc
-        "./src/**/*.{html,js}", // Nếu sau này bạn thêm file HTML hoặc JS trong src, Tailwind sẽ quét
-        "./node_modules/tw-elements/js/**/*.js",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: {
-            DEFAULT: 'red',   // cấu hình màu primary
-            100: '#990000'     // có thể cấu hình sâu hơn về màu sắc của primary
-          },
-         
+  content: [
+    "./index.html", // Quét file index.html ở thư mục gốc
+    "./src/**/*.{html,js}", // Nếu sau này bạn thêm file HTML hoặc JS trong src, Tailwind sẽ quét
+    "./node_modules/tw-elements/js/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        dm: ['"DM Sans"', 'sans-serif'],
+        inter: ['"Inter"', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          DEFAULT: 'red',   // cấu hình màu primary
+          100: '#990000'     // có thể cấu hình sâu hơn về màu sắc của primary
         },
+
       },
     },
-    plugins: [require("tw-elements/plugin.cjs")],
-    darkMode: "class"
-  };
+  },
+  plugins: [require("tw-elements/plugin.cjs")],
+  darkMode: "class"
+};
